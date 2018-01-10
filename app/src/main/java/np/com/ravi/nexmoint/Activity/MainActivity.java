82fe50a8bdd34e7deb47143e7e26ecae54649ef5
RaviGarbujaPun <cbpmgr@gmail.com>
2018-01-10 18:07:48 +0545

    Setup dependencies

---
+++ app/src/main/java/np/com/ravi/nexmoint/Activity/MainActivity.java
@@ -0,0 +1,30 @@
+package np.com.ravi.nexmoint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (TextUtils.isEmpty(Config.NexmoAppId) || TextUtils.isEmpty(Config.NexmoSharedSecretKey))
+            Toast.makeText(this, "Mandatory Nexmo config was not set.", Toast.LENGTH_LONG).show();
+        else {
+            setContentView(R.layout.activity_main);
+            if (savedInstanceState == null)
+                getFragmentManager().beginTransaction().add(R.id.container, new MainFragment()).commit();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+}
